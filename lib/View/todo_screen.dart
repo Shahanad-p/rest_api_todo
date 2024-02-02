35482bc28b462fedc26d5ba17d5104dd3ee94efc
Shahanad <shahanadp21@gmail.com>
2024-02-02 23:13:15 +0530

    done some ui things

--- lib/View/todo_screen.dart
+++ lib/View/todo_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:rest_api_todo_app/View/add_screen.dart';
 
 class ToDoListScreen extends StatefulWidget {
@@ -9,12 +12,33 @@ class ToDoListScreen extends StatefulWidget {
 }
 
 class _ToDoListScreenState extends State<ToDoListScreen> {
+  bool isLoading = true;
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
         centerTitle: true,
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => fetchData(),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index] as Map;
+            return ListTile(
+              title: Text(item['title']),
+              subtitle: Text(item['description']),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -24,5 +48,24 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> fetchData() async {
+    setState(() {
+      isLoading = false;
+    });
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
   }
 }
